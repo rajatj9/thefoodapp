f1c48088caaef24154701658f084fa7d18b7777d
Rajat Jain <jain.rajat50@gmail.com>
2019-04-13 21:06:05 +0800

    Achievements Card

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
-import { Gravatar, Card } from "nachos-ui";
-import ListItem from "../components/ListItem";
-import history from "../api/activity/history";
-import { Tabs, Tab, Container } from 'native-base'
+import { Gravatar } from "nachos-ui";
+import { Tabs, Tab, Container } from 'native-base';
+import Achievements from './History/Achievements';
+import PastOrders from './History/PastOrders';
 
 const gravatarStyle = {
   margin: 15
@@ -23,40 +22,17 @@ export default class SettingsScreen extends React.Component {
   };
 
   render() {
-    const cardStyle = { margin: 15, width: 280 };
-
     return (
-   
-        <Container>
+      <Container>
         <Tabs>
           <Tab heading={`Order History`}>
-          
-          <FlatList
-            data={history}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
-              <ListItem
-                key={item.id}
-                name={item.name}
-                image={`https://picsum.photos/280/280/?image=${9 + item.id}`}
-                cuisine={item.cuisine}
-                price={item.price}
-                label={item.restaurant}
-
-                handleNaviagation={() => {}}
-              />
-            )}
-          />
-          
-        </Tab>
-        <Tab heading={`Achievements`}>
-              <Text>
-                You are a pro!
-              </Text>
-        </Tab>
+            <PastOrders />
+          </Tab>
+          <Tab heading={`Achievements`}>
+            <Achievements />
+          </Tab>
         </Tabs>
-        </Container>
- 
+      </Container>
     );
   }
 }
