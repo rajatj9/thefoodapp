21a332d632ba883317c72029d8639e7a85252f68
piyushjha <piyushjha0999@gmail.com>
2019-04-13 20:58:58 +0800

    notifications added

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,20 +1,114 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  KeyboardAvoidingView,
+  View,} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { Permissions, Notifications } from 'expo';
 
 export default class LinksScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: null,
+      notification: null,
+      title: 'Hello World',
+      body: 'Say something!',
+    };
+  }
+
   static navigationOptions = {
     title: 'Links',
   };
 
+async registerForPushNotifications() {
+    const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  
+    if (status !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (status !== 'granted') {
+        return;
+      }
+    }
+  
+    const token = await Notifications.getExpoPushTokenAsync();
+  
+    this.subscription = Notifications.addListener(this.handleNotification);
+  
+    this.setState({
+      token,
+    });
+  }
+
+sendPushNotification(token = this.state.token, title = this.state.title, body = this.state.body) {
+    return fetch('https://exp.host/--/api/v2/push/send', {
+      body: JSON.stringify({
+        to: token,
+        title: title,
+        body: body,
+        data: { message: `${title} - ${body}` },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+  }
+
+handleNotification = notification => {
+    this.setState({
+      notification,
+    });
+  };
+
   render() {
+    
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
+      <KeyboardAvoidingView style={styles.container} behavior="position">
+      <Text style={styles.title}>Expo Sample Notifications App</Text>
+      <Text style={styles.text}>Title</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={title => this.setState({ title })}
+        maxLength={100}
+        value={this.state.title}
+      />
+      <Text style={styles.text}>Message</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={body => this.setState({ body })}
+        maxLength={100}
+        value={this.state.body}
+      />
+      <TouchableOpacity
+        onPress={() => this.registerForPushNotifications()}
+        style={styles.touchable}>
+        <Text>Register me for notifications!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => this.sendPushNotification()} style={styles.touchable}>
+        <Text>Send me a notification!</Text>
+      </TouchableOpacity>
+      {this.state.token ? (
+        <View>
+          <Text style={styles.text}>Token</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={token => this.setState({ token })}
+            value={this.state.token}
+          />
+        </View>
+      ) : null}
+      {this.state.notification ? (
+        <View>
+          <Text style={styles.text}>Last Notification:</Text>
+          <Text style={styles.text}>{JSON.stringify(this.state.notification.data.message)}</Text>
+        </View>
+      ) : null}
+    </KeyboardAvoidingView>
+      
+    );  
   }
 }
 
